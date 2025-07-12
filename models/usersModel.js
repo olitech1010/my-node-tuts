059afa94895930a06ca0ace128011e2640e79564
Clement Mensah <olitech1010@gmail.com>
2025-07-12 12:20:36 +0000

    user mode

---
+++ models/usersModel.js
@@ -0,0 +1,53 @@
+import { boolean } from "joi";
+import mongoose from "mongoose";
+
+const usersSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'email is required'],
+        unique: true,
+        minLength: [5, 'email must have at least 5 characters'],
+        trim: true,
+        lowercase:true
+        
+    },
+
+    password: {
+        type: String,
+        required: [true, 'password is required'],
+        minLength: [6, 'Password must be at least 6 characters'],
+        trim: true,
+        select: false,
+
+    },
+
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+
+    verificationCode: {
+        type: String,
+        select: false
+    },
+
+        verificationCodeValidation: {
+        type: Number,
+        select: false
+    },
+        
+        forgotPassswordCode: {
+        type: String,
+        select: false
+    },
+        forgotPassswordCodeValidation: {
+        type: Number,
+        select: false
+    }
+
+
+}, { timestamps: true })
+
+const User = mongoose.model('User', usersSchema)
+
+export default User
